cac276b962adfe22b0a09979e39bc35589518f91
mohdsuhaill <suhailabdmohammed@gmail.com>
2024-07-18 11:13:30 +0530

    completd

---
+++ Database/config.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoDB_URL = process.env.MONGODB_URL;
+
+const connectDB = async(req,res) =>{
+
+    try {
+        const connection = await mongoose.connect(mongoDB_URL);
+        console.log("MongoDB connected sucessfully")
+        return connection
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"MongoDb Conncetion Error"})
+    }
+};
+
+export default connectDB;
